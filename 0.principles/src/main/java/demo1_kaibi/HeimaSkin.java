9471dbff889fbc70886fc321c963f2a277a01348
Unicorn_acc <1049236690@qq.com>
2022-10-18 12:16:30 +0800

    init

---
+++ 0.principles/src/main/java/demo1_kaibi/HeimaSkin.java
@@ -0,0 +1,14 @@
+package demo1_kaibi;
+
+/**
+ * @version v1.0
+ * @ClassName: HeimaSkin
+ * @Description: 黑马程序员皮肤
+ * @Author: 黑马程序员
+ */
+public class HeimaSkin extends AbstractSkin {
+
+    public void display() {
+        System.out.println("黑马皮肤");
+    }
+}
